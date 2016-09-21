4bbf0a1e221b238696f45ae13904603f95017a57
Nigro Simone <nigro.simone@gmail.com>
2016-09-21 19:35:01 +0200

    Travis integration plus unit testing (#76)
    * unit test
    
    * Create .travis.yml
    
    * Create install-wp-tests.sh

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package W3_Total_Cache
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
