e761355984c98cea87f5e23456409338efcfd178
Nigro Simone <nigro.simone@gmail.com>
2016-09-25 16:56:40 +0200

    Update test-object-cache-file.php (#103)

--- tests/test-object-cache-file.php
+++ tests/test-object-cache-file.php
@@ -18,6 +18,19 @@ class W3_Object_Cache_File_Test extends W3_Object_Cache_Test {
         
        parent::setUp();
         
+       $cache_dir = WP_CONTENT_DIR . '/cache/';
+       
+       // try to create cache dir
+       if( !is_dir($cache_dir) ){
+           mkdir($cache_dir, 0777);
+       	   chmod($cache_dir, 0777);
+       }
+       
+       if( !is_dir($cache_dir) ){
+           $this->markTestSkipped('all tests in W3_Object_Cache_File_Test are invactive because cache directory "'.$cache_dir.'" not exists!');
+           return;
+       }
+        
        // set up for disk driver
        $this->config->set('dbcache.enabled', true);
        $this->config->set('objectcache.engine', 'file');
