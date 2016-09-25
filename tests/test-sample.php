b7d8980325578bba30945c979a0a85270d78bc4d
Nigro Simone <nigro.simone@gmail.com>
2016-09-25 11:50:54 +0200

    Update test-sample.php (#90)

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -5,16 +5,29 @@
  * @package W3_Total_Cache
  */
 
+require_once dirname(dirname(__FILE__)) . '/w3-total-cache.php';
+
 /**
  * Sample test case.
  */
 class SampleTest extends WP_UnitTestCase {
 
 	/**
+	 * @var W3_Root
+	 */
+	private $root;
+	
+	function setUp() {
+		$this->root = w3_instance('W3_Root');
+		$this->root->run();
+	}
+	
+	/**
 	 * A single example test.
 	 */
 	function test_sample() {
 		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+		$this->assertTrue( $this->root instanceof W3_Root );
 	}
 }
+
