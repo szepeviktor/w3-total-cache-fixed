f6dfdc324109bc59eb9616cd885811de7d7df56a
Kimberly <amiga-500@users.noreply.github.com>
2017-02-02 19:24:16 -0500

    Checkbox to Use CDN URLs for Media Library on Admin Pages (#350)
    This is a revision to #336 that moves away from using the "wp_prepare_attachment_for_js()" filter and attaches to "wp_get_attachment_url" and "attachment_link filters" instead.
    
    It also includes a new checkbox allowing the user to specify if they want to use CDN links instead of local ones for Media Library content on admin pages.

--- ConfigKeys.php
+++ ConfigKeys.php
@@ -1410,6 +1410,10 @@ $keys = array(
 		'type' => 'boolean',
 		'default' => false
 	),
+	'cdn.admin.media_library' => array(
+		'type' => 'boolean',
+		'default' => false
+	),
 	'varnish.configuration_overloaded' => array(
 		'type' => 'boolean',
 		'default' => false
