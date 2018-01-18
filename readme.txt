14232d136993e5498d45141cb3b49096aa067037
nigrosimone <nigro.simone@gmail.com>
2018-01-18 19:50:40 +0100

    Readying a new release

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: fredericktownes
 Tags: seo, cache, caching, compression, maxcdn, nginx, varnish, redis, new relic, aws, amazon web services, s3, cloudfront, rackspace, cloudflare, azure, apache
 Requires at least: 3.2
 Tested up to: 4.9.1
-Stable tag: 0.9.6
+Stable tag: 0.9.6.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- w3-total-cache.php
+++ w3-total-cache.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: W3 Total Cache (Fixed)
 Description: The highest rated and most complete WordPress performance plugin. Dramatically improve the speed and user experience of your site. Add browser, page, object and database caching as well as minify and content delivery network (CDN) to WordPress.
-Version: 0.9.6
+Version: 0.9.6.1
 Plugin URI: https://github.com/szepeviktor/w3-total-cache-fixed/
 Author: W3TC (Fixed) Community
 Author URI: https://github.com/szepeviktor/w3-total-cache-fixed/
